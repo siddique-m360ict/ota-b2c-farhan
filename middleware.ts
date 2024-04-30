839e48361dd29ec70478c3a9f45c147525d9d7e0
Amanullah-M360ict <amanullah.m360ict@gmail.com>
2024-04-30 13:30:51 +0600

    try to fix

--- middleware.ts
+++ middleware.ts
@@ -6,38 +6,38 @@ import { GetProfile } from "./lib/server/auth/UserProfileEndpoints"
 export default withAuth(
   async function middleware(req) {
     const token = req.cookies.get("b_token")?.value
-    if (!!token) {
-      const res = await GetProfile(token as string)
-      const isAuth = !!res.success
+    // if (!!token) {
+    //   const res = await GetProfile(token as string)
+    //   const isAuth = !!res.success
 
-      const isAuthPage =
-        req.nextUrl.pathname.startsWith("/login") ||
-        req.nextUrl.pathname.startsWith("/register")
+    //   const isAuthPage =
+    //     req.nextUrl.pathname.startsWith("/login") ||
+    //     req.nextUrl.pathname.startsWith("/register")
 
-      console.log(`middleware call...................`)
+    //   console.log(`middleware call...................`)
 
-      if (isAuthPage && isAuth) {
-        return NextResponse.redirect(new URL("/", req.url))
-      }
+    //   if (isAuthPage && isAuth) {
+    //     return NextResponse.redirect(new URL("/", req.url))
+    //   }
 
-      if (!isAuth && !isAuthPage) {
-        let from = req.nextUrl.pathname
-        if (req.nextUrl.search) {
-          from += req.nextUrl.search
-        }
+    //   if (!isAuth && !isAuthPage) {
+    //     let from = req.nextUrl.pathname
+    //     if (req.nextUrl.search) {
+    //       from += req.nextUrl.search
+    //     }
 
-        return NextResponse.redirect(
-          new URL(`/login?from=${encodeURIComponent(from)}`, req.url)
-        )
-      }
-    } else {
-      const isAuthPage =
-        req.nextUrl.pathname.startsWith("/login") ||
-        req.nextUrl.pathname.startsWith("/register")
-      if (!isAuthPage) {
-        return NextResponse.redirect(new URL(`/login`, req.url))
-      }
-    }
+    //     return NextResponse.redirect(
+    //       new URL(`/login?from=${encodeURIComponent(from)}`, req.url)
+    //     )
+    //   }
+    // } else {
+    //   const isAuthPage =
+    //     req.nextUrl.pathname.startsWith("/login") ||
+    //     req.nextUrl.pathname.startsWith("/register")
+    //   if (!isAuthPage) {
+    //     return NextResponse.redirect(new URL(`/login`, req.url))
+    //   }
+    // }
   },
   {
     callbacks: {
