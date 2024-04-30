a34fdc74458a63e63115f39802579fa52293161f
Amanullah-M360ict <amanullah.m360ict@gmail.com>
2024-04-30 14:58:32 +0600

    some change

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,28 +1,55 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { NextAuthOptions } from "next-auth"
-import EmailProvider from "next-auth/providers/email"
-import GitHubProvider from "next-auth/providers/github"
-
-import { env } from "@/env.mjs"
-import { siteConfig } from "@/config/site"
-
+import CredentialsProvider from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google"
 export const authOptions: NextAuthOptions = {
   providers: [
-    GitHubProvider({
-      clientId: env.GITHUB_CLIENT_ID,
-      clientSecret: env.GITHUB_CLIENT_SECRET,
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        user_email: {
+          label: "Email",
+          type: "email",
+          placeholder: "jsmith@yahoo.com",
+        },
+        user_password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        return null
+      },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  // secret: process.env.NEXTAUTH_SECRET,
+  // session: { strategy: 'jwt' },
   callbacks: {
-    async session({ token, session }) {
-      if (token) {
-        session.user.id = token.id
-        session.user.name = token.name
-        session.user.email = token.email
-        session.user.image = token.picture
+    async signIn({ account, profile }) {
+      if (account?.provider === "google") {
       }
+      return true
+    },
+    async jwt({ token, user, account, trigger, session }) {
+      if (trigger === "update") {
+        return { ...token, ...session }
+      }
+      if (account?.provider === "google") {
+        const email = user?.email
+        const id = user?.id
+        const name = user?.name
 
+        token.user_id = 1
+      }
+      return { ...token, ...user }
+    },
+    async session({ session, token, user }) {
+      session.user = token as any
       return session
     },
+  },
+  pages: {
+    signIn: "/login",
+    newUser: "/register",
   },
 }
