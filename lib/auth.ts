35092712389a29a8fd9f59d6ac9ce7fd696d728b
Amanullah-M360ict <amanullah.m360ict@gmail.com>
2024-04-30 14:42:54 +0600

    still fix issue

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,26 +1,18 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { NextAuthOptions } from "next-auth"
 import EmailProvider from "next-auth/providers/email"
 import GitHubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google"
-import type { Provider } from "next-auth/providers/index"
+
 import { env } from "@/env.mjs"
 import { siteConfig } from "@/config/site"
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    env.GITHUB_CLIENT_ID &&
-      env.GITHUB_CLIENT_SECRET &&
-      GitHubProvider({
-        clientId: env.GITHUB_CLIENT_ID,
-        clientSecret: env.GITHUB_CLIENT_SECRET,
-      }),
-    env.GOOGLE_CLIENT_ID &&
-      env.GOOGLE_CLIENT_SECRET &&
-      GoogleProvider({
-        clientId: env.GOOGLE_CLIENT_ID,
-        clientSecret: env.GOOGLE_CLIENT_SECRET,
-      }),
-  ].filter(Boolean) as Provider[],
+    GitHubProvider({
+      clientId: env.GITHUB_CLIENT_ID,
+      clientSecret: env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
   callbacks: {
     async session({ token, session }) {
       if (token) {
@@ -29,10 +21,8 @@ export const authOptions: NextAuthOptions = {
         session.user.email = token.email
         session.user.image = token.picture
       }
+
       return session
     },
-  },
-  pages: {
-    signIn: "/malik",
   },
 }
