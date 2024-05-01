959638508ec8cfb244fe4ba486da812ae9bc61a5
Amanullah-M360ict <amanullah.m360ict@gmail.com>
2024-05-01 22:30:15 +0600

    reset prev commit

--- app/(searchFlight)/flightsearch/page.tsx
+++ app/(searchFlight)/flightsearch/page.tsx
@@ -3,10 +3,9 @@ import FlightListView from "@/components/flight-search/FlightListView"
 import React from "react"
 import dynamic from "next/dynamic"
 import BoxLoader from "@/components/flight-search/elements/BoxLoader"
-import GetFlightList_V2 from "./actions"
 
 const page = async ({ params, searchParams }) => {
-  const res = await GetFlightList_V2(searchParams)
+  const res = await getAllFlights()
   console.log(res)
   return <FlightListView flights={res?.data} count={res.count} />
 }
