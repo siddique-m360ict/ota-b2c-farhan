01112dbae334b81c304507a340b7c6f495154fdb
Amanullah-M360ict <amanullah.m360ict@gmail.com>
2024-05-01 18:09:26 +0600

    try to fix

--- app/(flightsSearch)/flight-search/page.tsx
+++ app/(flightsSearch)/flight-search/page.tsx
@@ -1,17 +1,25 @@
 import React from "react"
 import dynamic from "next/dynamic"
 import BoxLoader from "@/components/flight-search/elements/BoxLoader"
-import GetFlightList_V2 from "./actions"
+import { getAllFlights } from "./actions"
 
-const FlightView = dynamic(() => import("./FlightListView"), {
-  loading: () => <BoxLoader message="Loading UI..." />,
-})
+const FlightView = dynamic(
+  () => import("../../../components/flight-search/FlightListView"),
+  {
+    loading: () => <BoxLoader message="Loading UI..." />,
+  }
+)
 
 async function Page({ params, searchParams }) {
-  const res = await GetFlightList_V2(searchParams)
+  const res = await getAllFlights()
   console.log(res)
 
-  return <FlightView flights={res?.data} count={res.count} />
+  return (
+    <div>
+      <h1>{res?.message}</h1>
+      <FlightView flights={res?.data} count={res.count} />
+    </div>
+  )
 }
 
 export default Page
