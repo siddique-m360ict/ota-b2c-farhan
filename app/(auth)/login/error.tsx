65a56800fedf8dd6c1fedf7d8b88f8aee8f15981
Amanullah-M360ict <amanullah.m360ict@gmail.com>
2024-05-02 15:47:02 +0600

    try to fix

--- app/(auth)/login/error.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-const ErrorBoundary = ({ error }: { error: Error }) => {
-  return <div>login: {error.message}</div>
-}
-
-export default ErrorBoundary
--- app/(searchFlight)/flightsearch/page.tsx
+++ app/(searchFlight)/flightsearch/page.tsx
@@ -3,17 +3,17 @@ import FlightListView from "@/components/flight-search/FlightListView"
 import React from "react"
 import dynamic from "next/dynamic"
 import BoxLoader from "@/components/flight-search/elements/BoxLoader"
+import FlightDataGet from "@/components/flight-search/FlightDataGet"
+
+const FlightView = dynamic(
+  () => import("../../../components/flight-search/FlightDataGet"),
+  {
+    loading: () => <BoxLoader message="Loading UI..." />,
+  }
+)
 
 const page = async ({ params, searchParams }) => {
-  if (Object.keys(searchParams).length !== 0) {
-    console.log({ searchParams })
-  }
-  console.log({
-    p: "dhbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbfs",
-    condition: Object.keys(searchParams).length !== 0,
-  })
-
-  return <div>response: </div>
+  return <FlightView searchParams={searchParams} />
 }
 
 export default page
