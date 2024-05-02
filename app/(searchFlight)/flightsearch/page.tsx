80032d034e62103f4f67dd0d0a3adf6e99ca83c9
Amanullah-M360ict <amanullah.m360ict@gmail.com>
2024-05-02 16:28:12 +0600

    Suspense add

--- app/(searchFlight)/flightsearch/page.tsx
+++ app/(searchFlight)/flightsearch/page.tsx
@@ -1,11 +1,14 @@
 import { getAllFlights } from "@/app/actions"
 import FlightListView from "@/components/flight-search/FlightListView"
-import React from "react"
+import React, { Suspense } from "react"
 import dynamic from "next/dynamic"
 
 const page = async ({ params, searchParams }) => {
-  console.log(searchParams)
-  return <div>FlightDataGet {searchParams?.departuredate} </div>
+  return (
+    <Suspense fallback={<>loading...................</>}>
+      <div>searchParams date is: {searchParams?.departuredate}</div>
+    </Suspense>
+  )
 }
 
 export default page
