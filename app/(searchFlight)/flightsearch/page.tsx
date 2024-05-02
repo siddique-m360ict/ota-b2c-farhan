21fbf50dca1c22dab3b0305377b7ca901c56b1f1
Amanullah-M360ict <amanullah.m360ict@gmail.com>
2024-05-02 15:57:40 +0600

    ssr check

--- app/(searchFlight)/flightsearch/page.tsx
+++ app/(searchFlight)/flightsearch/page.tsx
@@ -2,18 +2,10 @@ import { getAllFlights } from "@/app/actions"
 import FlightListView from "@/components/flight-search/FlightListView"
 import React from "react"
 import dynamic from "next/dynamic"
-import BoxLoader from "@/components/flight-search/elements/BoxLoader"
-import FlightDataGet from "@/components/flight-search/FlightDataGet"
-
-const FlightView = dynamic(
-  () => import("../../../components/flight-search/FlightDataGet"),
-  {
-    loading: () => <BoxLoader message="Loading UI..." />,
-  }
-)
 
 const page = async ({ params, searchParams }) => {
-  return <FlightView searchParams={searchParams} />
+  console.log(searchParams)
+  return <div>FlightDataGet {searchParams?.departuredate} </div>
 }
 
 export default page
