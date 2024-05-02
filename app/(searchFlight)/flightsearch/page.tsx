340f05fff83e5b5052a4a553011dc4924c2c5c1e
Amanullah-M360ict <amanullah.m360ict@gmail.com>
2024-05-02 17:18:18 +0600

    searchParams issue

--- app/(searchFlight)/flightsearch/page.tsx
+++ app/(searchFlight)/flightsearch/page.tsx
@@ -3,14 +3,12 @@ import FlightListView from "@/components/flight-search/FlightListView"
 import React, { Suspense } from "react"
 import dynamic from "next/dynamic"
 
-const page = async ({ params, searchParams }) => {
-  console.log(searchParams)
-
-  return (
-    <Suspense fallback={<>loading...................</>}>
-      <div>searchParams date is: </div>
-    </Suspense>
-  )
+export default function Page({
+  params,
+  searchParams,
+}: {
+  params: { slug: string }
+  searchParams?: { [key: string]: string | string[] | undefined }
+}) {
+  return <h1>{searchParams?.departuredate || "Hello!"}</h1>
 }
-
-export default page
