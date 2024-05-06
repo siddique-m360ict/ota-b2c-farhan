a14d009ba2a28da4febef9e4977904f0972b7f94
Amanullah-M360ict <amanullah.m360ict@gmail.com>
2024-05-06 21:56:21 +0600

    stream query check

--- src/components/home/elements/OneWay.tsx
+++ src/components/home/elements/OneWay.tsx
@@ -54,7 +54,7 @@ const OneWay = ({ cabinClass, passenger }: Props) => {
       newParams.set("infant", passenger.infant.toString())
     newParams.set("class", cabinClass)
     newParams.set("route", "oneway")
-    router.push(createUrl("/flightsearch", newParams))
+    router.push(createUrl("/stream", newParams))
   }
 
   return (
