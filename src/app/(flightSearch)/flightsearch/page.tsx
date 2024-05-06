bb823f4896cc30a2380601321e2eb03ec4c4dc75
Amanullah-M360ict <amanullah.m360ict@gmail.com>
2024-05-06 13:42:54 +0600

    Suspense issue try to fix

--- src/app/(flightSearch)/flightsearch/page.tsx
+++ src/app/(flightSearch)/flightsearch/page.tsx
@@ -3,17 +3,30 @@ import { getAllFlights } from "../actions"
 import FlightListView from "@/components/flight-search/FlightListView"
 import CardLoader from "@/components/flight-search/elements/CardLoader"
 import LoadingIndicator from "@/components/common/spinner/LoadingIndicator"
+import { unstable_noStore } from "next/cache"
+import { HTTPResponse } from "@/lib/commonTypes"
+import { IFlightSearchList } from "@/components/home/elements/types/flightSearchType"
 
 const FlightSearchPage = async ({ params, searchParams }) => {
-  const keyString = JSON.stringify(searchParams)
+  const origin = searchParams.origin
+  const destination = searchParams.destination
+  const departuredate = searchParams.departuredate
+  const adults = searchParams.adults
+  const cabin = searchParams.class
+  const route = searchParams.route
+
   return (
-    <Suspense key={keyString} fallback={<CardLoader numberFlight={8} />}>
+    <Suspense
+      key={origin + destination + departuredate + adults + cabin + route}
+      fallback={<CardLoader numberFlight={8} />}
+    >
       <AllFlights searchParams={searchParams} />
     </Suspense>
   )
 }
 
 const AllFlights = async (props: any) => {
+  unstable_noStore()
   const res = await getAllFlights(props?.searchParams)
   return (
     <div>
