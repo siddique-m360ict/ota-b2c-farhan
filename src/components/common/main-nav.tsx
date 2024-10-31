1eb2f755bfe2b07ce06848c81752ab42b5bba11f
siddique-m360ict <siddique.m360ict@gmail.com>
2024-10-31 16:27:54 +0600

    login fixed

--- src/components/common/main-nav.tsx
+++ src/components/common/main-nav.tsx
@@ -3,22 +3,21 @@
 import React, { useState, useEffect } from "react"
 import Link from "next/link"
 import { useSelectedLayoutSegment } from "next/navigation"
-import {
-  Bell,
-  Home,
-  Plane,
-  Train,
-  Car,
-  Map,
-  Sun,
-  Moon,
-  CircleUserRound,
-} from "lucide-react"
+import { CircleUserRound } from "lucide-react"
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
+import WarningOTP from './WarningOTP'
+import { useAppSelector } from "@/lib/redux/hooks"
+import { getCookies } from "cookies-next"
+import Image from "next/image"
+import { UserAccountNav } from './user-account-nav';
+import { ModeToggle } from './mode-toggle';
 
 const MainNav = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false)
   const [isScrolled, setIsScrolled] = useState(false)
   const segment = useSelectedLayoutSegment()
+  const user = useAppSelector((state) => state.user)
+  const isLogin = getCookies().b_token
 
   // Handle scroll effect
   useEffect(() => {
@@ -67,18 +66,18 @@ const MainNav = () => {
       <div className="mx-auto flex max-w-7xl items-center justify-between p-4">
         {/* Logo */}
         <div className="flex items-center gap-8">
-          <Link href="/" className="text-3xl font-bold text-white">
+          <Link href="/" className="text-2xl font-bold text-white">
             Farhan Travels
           </Link>
 
           {/* Navigation Items */}
           <nav className="hidden md:block">
-            <ul className="ms-12 flex items-center gap-12">
+            <ul className="flex items-center gap-12">
               {navItems.map((item) => (
                 <li key={item.id}>
                   <Link
                     href={item.href}
-                    className={`ms-3 flex items-center gap-2 text-white transition-colors hover:text-gray-200 hover:underline
+                    className={`flex items-center gap-2 text-white transition-colors hover:text-gray-200 hover:underline
                       ${
                         item.href.startsWith(`/${segment}`)
                           ? "text-yellow-300"
@@ -95,17 +94,30 @@ const MainNav = () => {
 
         {/* Right Side Controls */}
         <div className="flex items-center gap-4">
-          <div className="flex gap-4 text-white">
-            <Link href="/login" className="flex items-center font-bold hover:text-gray-200 ">
-              <CircleUserRound className="mx-1 size-4" /> Sign In
-            </Link>
-            <Link
-              href="/register"
-              className="rounded bg-white/25 px-2 py-1 font-bold transition-colors hover:bg-white/30 "
-            >
-              Register
-            </Link>
-          </div>
+          {isLogin && user?.success ? (
+            <div className="flex items-center gap-6">
+              {!user.data.is_verified && <WarningOTP />}
+              <ModeToggle />
+              <UserAccountNav />
+            </div>
+          ) : (
+            <div className="flex items-center gap-4">
+              <ModeToggle />
+              <Link
+                href="/login"
+                className="flex items-center font-bold text-white hover:text-gray-200"
+              >
+                <CircleUserRound className="mr-2 size-4" />
+                Sign In
+              </Link>
+              <Link
+                href="/register"
+                className="rounded bg-white/25 px-4 py-1.5 font-bold text-white transition-colors hover:bg-white/30"
+              >
+                Register
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </div>
